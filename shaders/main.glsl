b877bf2e9c3547a82505901052d6016166643fb3
phoenixbf <phoenixbf@gmail.com>
2018-02-01 15:24:33 +0100

    updates

--- shaders/main.glsl
+++ shaders/main.glsl
@@ -209,13 +209,14 @@ float getEmissionWeight(){
 // Env Fetch
 vec3 envmapReflectionVector(const in mat4 transform, const in vec3 view, const in vec3 normal){
     vec3 lv = reflect(view, normal);
-    lv = normalize(lv);
+    //lv = normalize(lv);
 
     vec3 x = vec3(transform[0][0], transform[1][0], transform[2][0]);
     vec3 y = vec3(transform[0][1], transform[1][1], transform[2][1]);
     vec3 z = vec3(transform[0][2], transform[1][2], transform[2][2]);
     mat3 m = mat3(x,y,z);
-    return m*lv;
+
+    return normalize(m*lv);
 }
 vec3 envmapVector(const in mat4 transform, const in vec3 normal){
     vec3 lv = -normal;
@@ -224,7 +225,8 @@ vec3 envmapVector(const in mat4 transform, const in vec3 normal){
     vec3 y = vec3(transform[0][1], transform[1][1], transform[2][1]);
     vec3 z = vec3(transform[0][2], transform[1][2], transform[2][2]);
     mat3 m = mat3(x,y,z);
-    return m*lv;
+    
+    return normalize(m*lv);
 }
 
 // Returns (start, end, roughness-norm-offs, v-flip)

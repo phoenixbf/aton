742d2ccfee076e6f5dabb02682ad14c2204c46e8
phoenixbf <phoenixbf@gmail.com>
2018-10-13 03:50:21 +0200

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,57 @@
 module.exports = {
-    apps: [
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
     {
-        name: "content-service",
-        script: "services/ATON.SERVICE.vroadcast.js",
-        instances: "max",
-        exec_mode: "cluster",
-        args: "--www ."
+      name      : 'content',
+      script    : 'services/ATON.SERVICE.vroadcast.js',
+      args      : '--www ./',
+      instances : 'max',
+      exec_mode : 'cluster',
+
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
     },
+
+    // Second application
     {
-        name: "collab-service"
-        script: "./services/ATON.SERVICE.vroadcast.js",
-        instances: "1",
-    }]
-}
+      name      : 'collab',
+      script    : 'services/ATON.SERVICE.vroadcast.js',
+      instances : 1,
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};

9b28cb12ab3d44665aa4d3966771318eeae9ba25
phoenixbf <phoenixbf@gmail.com>
2020-04-19 13:03:35 +0200

    fix paths

---
+++ services/deploy-pm2.sh
@@ -0,0 +1,2 @@
+pm2 start ecosystem.config.js
+pm2 start -x ATON.SERVICE.atonizer.js
---
+++ services/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+  apps: [
+    // Content service
+    {
+      name      : 'Content Service',
+      script    : 'ATON.SERVICE.content.js',
+      instances : 'max',
+      exec_mode : 'cluster',
+    },
+
+    // VRoadcast service
+    {
+      name      : 'VRoadcast Service',
+      script    : 'ATON.SERVICE.vroadcast.js',
+      instances : 1,
+      exec_mode : 'cluster'
+    }
+  ]
+};

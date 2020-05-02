db0ce738573d1a1b9e5347e60fcfcc430993434b
Bruno Fanini <phoenixbf@gmail.com>
2020-05-02 19:14:00 +0200

    tests

--- js/ATON.vroadcast.js
+++ js/ATON.vroadcast.js
@@ -265,14 +265,14 @@ ATON.vroadcast.connect = function(address, scene, bSecure){
     else ATON.vroadcast._scene = "_SHARED_";
 
     if (address === undefined) return; //ATON.vroadcast.socket = io();
-    
+    /*
     if (bSecure) 
         ATON.vroadcast.socket = io.connect("https://"+address+":"+ATON.vroadcast.PORT_SECURE+"/", {
             //transports: ['websocket'], 
             secure: true, 
             rejectUnauthorized: false
         });
-    else 
+    else */
         ATON.vroadcast.socket = io.connect("http://"+address+":"+ATON.vroadcast.PORT+"/");
 
     ATON.vroadcast.connected = ATON.vroadcast.socket.connected;

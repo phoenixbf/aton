0a50d36913b010e2ab45fd54b486fd46d121854e
Bruno Fanini <phoenixbf@gmail.com>
2020-10-04 15:49:36 +0200

    API documentation update

--- public/src/ATON.nav.js
+++ public/src/ATON.nav.js
@@ -606,14 +606,12 @@ Nav.requestHome = (duration)=>{
 };
 
 
-
-// Mobile devori (modified for absolute compass - UNSTABLE)
-/**
- * @author richt / http://richt.me
- * @author WestLangley / http://github.com/WestLangley
- *
- * W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
- */
+/*
+Mobile devori (modified for absolute compass - UNSTABLE)
+author richt / http://richt.me
+author WestLangley / http://github.com/WestLangley
+W3C Device Orientation control (http://w3c.github.io/deviceorientation/spec-source-orientation.html)
+*/
 Nav.DeviceOrientationControls = function ( object ) {
     let scope = this;
 

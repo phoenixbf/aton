22c597ec313a56dcdd3de03bc505e113945f2f5d
Bruno Fanini <phoenixbf@gmail.com>
2022-05-25 10:14:51 +0200

    UI updates

--- public/src/ATON.ui.js
+++ public/src/ATON.ui.js
@@ -17,15 +17,34 @@ UI.init = ()=>{
 
 
 UI.basicSetup = ()=>{
+/*
     UI.addButton("idTopToolbar","fullscreen", ATON.toggleFullScreen );
     if (ATON.Utils.isConnectionSecure()) UI.addButton("idTopToolbar","vr", ATON.XR.toggle );
 
     UI.addButton("idBottomToolbar","home", ()=>{ ATON.Nav.requestHome(0.1); });
+*/
 };
 
 // TODO:
-UI.button = ()=>{
+UI.button = (id, onpress, icon, text, tooltip)=>{
+    if (icon && !icon.includes(".")) icon = ATON.FE.PATH_RES_ICONS+icon+".png";
 
+    let domEL = document.createElement('div');
+    if (id) domEL.id = id;
+
+    if (text) domEL.className = "atonBTN atonBTN-text";
+    else domEL.className = "atonBTN";
+
+    if (icon) domEL.innerHTML = "<img src='"+icon+"'>";
+    if (text) domEL.innerHTML += text;
+
+    if (tooltip) domEL.title = tooltip;
+
+    let el = $(domEL);
+
+    if (onpress) el.bind("click", onpress);
+
+    return el;
 };
 
 

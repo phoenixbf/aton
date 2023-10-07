e13951dbb3e40bacd1339a7cef210dcdcb914d0b
Bruno Fanini <phoenixbf@gmail.com>
2023-10-07 20:26:47 +0200

    PM2 ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,14 @@
 module.exports = {
 
   apps: [
-    // Main service
+    // Main service (gateway)
     {
       name          : 'ATON Main Service',
       script        : 'services/ATON.service.main.js',
       instances     : 'max',
       exec_mode     : 'cluster',
       watch         : ["services","config"],
+      ignore_watch  : ["config/flares"], 
       instance_var  : 'INSTANCE_ID',
       merge_logs    : true,
       //restart_delay : 1000,
@@ -17,13 +18,14 @@ module.exports = {
       }
     },
 
-    // VRoadcast service
+    // Photon service
     {
       name          : 'ATON Photon Service',
       script        : 'services/photon/ATON.service.photon.js',
       instances     : 1,
       exec_mode     : 'cluster',
       watch         : ["services","config"],
+      ignore_watch  : ["config/flares"],
       merge_logs    : true,
       //restart_delay : 1000,
       //out_file     : "./logs/ATON.service.photon.log",
@@ -39,6 +41,7 @@ module.exports = {
       instances     : 1,
       exec_mode     : 'cluster',
       watch         : ["services", "config"],
+      ignore_watch  : ["config/flares"],
       merge_logs    : true,
       //restart_delay : 1000,
       //out_file     : "./logs/ATON.service.webdav.log",

ee99b187591408ac230d3a4bc4d00eb823976b9c
Bruno Fanini <phoenixbf@gmail.com>
2024-11-08 15:42:38 +0100

    ATON.UI first samples

--- public/src/ATON.ui.js
+++ public/src/ATON.ui.js
@@ -14,10 +14,10 @@ A set UI blueprints for ATON apps, based on Bootstrap v5
 */
 let UI = {};
 
-//UI.POPUP_DT = 500; //300;
-
 
 UI.init = ()=>{
+    if (!bootstrap) return;
+
     UI.PATH_RES_ICONS = ATON.PATH_RES+"icons/";
 
     UI._setupBase();
@@ -30,25 +30,32 @@ UI._createElemementFromHTMLString = (html)=>{
     return container.firstElementChild;
 };
 
+// This can be possibly replaced by custom user routine
+UI.onContextMenu = ()=>{
+    return false;
+};
+
 // Setup base structure
 UI._setupBase = ()=>{
-    document.body.oncontextmenu = ()=>{ return false; };
+    document.body.oncontextmenu = UI.onContextMenu;
 
-    // Modal dialog
+    // Defaults to dark
+    document.body.setAttribute("data-bs-theme","dark");
+
+    // Centralized modal dialog
     //UI.elModal = document.createElement('div');
     UI.elModal = UI._createElemementFromHTMLString(`
         <div class="modal fade modal-fullscreen-md-down" id="staticBackdrop" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                <div class="modal-content">
-                    <div id="uiModalHeader" class="modal-header"></div>
-                    <div id="uiModalBody" class="modal-body"></div>
-                    <div id="uiModalFooter" class="modal-footer"></div>
-                </div>
+                <div class="modal-content aton-std-bg" id="uiModalContent"></div>
             </div>
         </div>
 	`);
 
+    UI.modal = new bootstrap.Modal(UI.elModal);
+
     document.body.append(UI.elModal);
+    UI.elModalContent = document.getElementById("uiModalContent");
 
 /*
 
@@ -93,6 +100,48 @@ UI._setupBase = ()=>{
 */
 };
 
+/*===============================
+    Main Modal (popup)
+===============================*/
+UI.showModal = (options)=>{
+
+    // Clear
+    UI.elModalContent.innerHTML = "";
+
+    if (options.header){
+        let el = document.createElement('div');
+        el.classList.add("modal-header");
+        //el.append(options.header);
+
+        el.innerHTML = "<h4 class='modal-title' id='staticBackdropLabel'>"+options.header+"</h4>";
+
+        UI.elModalContent.append( el );
+    }
+    if (options.body){
+        let el = document.createElement('div');
+        el.classList.add("modal-body");
+        el.append(options.body);
+
+        UI.elModalContent.append( el );
+    }
+    if (options.footer){
+        let el = document.createElement('div');
+        el.classList.add("modal-footer");
+        el.append(options.footer);
+
+        UI.elModalContent.append( el );
+    }
+
+    UI.modal.show();
+};
+
+UI.closeModal = ()=>{
+    UI.modal.hide();
+};
+
+/*===============================
+    Utilities
+===============================*/
 UI.loadPartial = (src, elParent, bPrepend)=>{
     $.get(src, (data)=>{
         if (!parentid){
@@ -119,5 +168,34 @@ UI.hideSemLabel = ()=>{
     $(UI._elLabel).html("");
 };
 */
+
+/*===============================
+    Items
+===============================*/
+
+UI.createButton = (options)=>{
+    let el = document.createElement('button');
+    el.classList.add("btn", "aton-btn");
+    el.setAttribute("type","button");
+
+    if (options.variant) el.classList.add("btn-"+options.variant); // Bootstrap button variants (primary, info, ...)
+
+    if (options.text) el.innerText = options.text;
+
+    if (options.icon){
+        let iconsrc = UI.PATH_RES_ICONS + options.icon+".png";
+        if (options.icon.includes("/")) iconsrc = options.icon; // a path is given
+
+        el.prepend( UI._createElemementFromHTMLString("<img class='icon' src='"+iconsrc+"'>"));
+    }
+
+    if (options.onpress) el.onclick = options.onpress;
+
+    return el;
+};
+
+UI.createCard = (options)=>{
+
+};
 
 export default UI;
